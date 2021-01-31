39881d6a43722785a565c6f2e3572395b5ecb40d
luorenjie <lrj193927@gmail.com>
2021-01-30 21:09:42 -0500

    add resilience4J circuit breaker

--- src/main/java/com/sfg/gateway/config/LocalBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalBalancedRoutesConfig.java
@@ -18,7 +18,13 @@ public class LocalBalancedRoutesConfig {
                 .route(r -> r.path("/api/v1/customers*", "/api/v1/customers/*/orders", "/api/v1/customers/*/orders/*")
                         .uri("lb://beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                        .setFallbackUri("forward:/inventory-failover")
+                                        .setRouteId("inv-failover")
+                        ))
                         .uri("lb://beer-inventory-service"))
+                .route(r -> r.path("/inventory-failover*")
+                .uri("lb://beer-inventory-failover"))
                 .build();
     }
 
